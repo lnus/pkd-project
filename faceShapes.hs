bb6b9ecbd801e582a872681132dc40a6e375e063
dali0342 <david.lide.0342@student.uu.se>
2022-02-28 12:09:18 +0100

    1 more faceshape

--- faceShapes.hs
+++ faceShapes.hs
@@ -1,11 +1,10 @@
 import Graphics.Gloss
 
-main = display FullScreen white eggshape
-
-faceshapes = pictures [eggshape,diamondshape,squareshape]
+faceshapes = pictures [eggshape,diamondshape,squareshape,roundshape]
 
 eggshape = polygon [(0,500),(250,450),(350,300),(375,150),(375,0),(350,-150),(300,-300),(200,-400),(50,-500),
                     (-50,-500),(-200,-400),(-300,-300),(-350,-150),(-375,0),(-375,150),(-350,300),(-250,450),(0,500)]
-roundshape   = undefined
+roundshape   = circleSolid 500
 diamondshape = polygon [(0,500),(500,0),(0,-500),(-500,0)]
 squareshape  = polygon [(-500,500),(500,500),(500,-500),(-500,-500)]
+
