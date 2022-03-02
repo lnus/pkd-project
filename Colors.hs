2b282ed739dd09391cd6461805906423bf6673a1
victorodeback <victorodebck@gmail.com>
2022-03-02 10:39:55 +0100

    Added color-file.

---
+++ Colors.hs
@@ -0,0 +1,12 @@
+module Colors where
+import Graphics.Gloss
+
+lightSkin = makeColorI 255 230 216 255
+lighterSkin = makeColorI 239 198 173 255
+darkerSkin = makeColorI 198 143 109 255
+darkSkin = makeColorI 118 77 52 255
+
+lightBlue = makeColorI 113 132 182 255
+lightGreen = makeColorI 100 121 102 255
+brown = makeColorI 70 46 33 255
+
