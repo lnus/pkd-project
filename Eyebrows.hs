d0077634a04d2686539d1fe3e5a0d0dcec5b979b
victorodeback <victorodebck@gmail.com>
2022-03-04 11:03:39 +0100

    Fixed eyebrow placement.

--- Eyebrows.hs
+++ Eyebrows.hs
@@ -16,34 +16,26 @@ eyebrows = pictures [normalbrow, thinbrow, straightbrow]
 
 normalbrow = pictures [lNormalbrow, rNormalbrow]
 
-rNormalbrow = pictures [rNormalbrowLeft, rNormalbrowRight]
-
+rNormalbrow = translate 50 0 (pictures [rNormalbrowLeft, rNormalbrowRight])
 rNormalbrowLeft = polygon [(0, 0), (0, 45), (100, 70), (120, 70), (120, 20), (100, 20), (60, 10), (0, 0)]
-
 rNormalbrowRight = polygon [(120, 70), (200, 55), (250, 35), (275, 20), (300, 0), (200, 20), (120, 20)]
 
-lNormalbrow = pictures [lNormalbrowLeft, lNormalbrowRight]
-
+lNormalbrow = translate (-50) 0 (pictures [lNormalbrowLeft, lNormalbrowRight])
 lNormalbrowRight = polygon [(0, 0), (0, 45), (-100, 70), (-120, 70), (-120, 20), (-100, 20), (-60, 10), (0, 0)]
-
 lNormalbrowLeft = polygon [(-120, 70), (-200, 55), (-250, 35), (-275, 20), (-300, 0), (-200, 20), (-120, 20)]
 
 thinbrow = pictures [lThinbrow, rThinbrow]
 
-rThinbrow = pictures [rThinbrowLeft, rThinbrowRight]
-
+rThinbrow = translate 50 0 (pictures [rThinbrowLeft, rThinbrowRight])
 rThinbrowLeft = polygon [(0, 0), (0, 20), (100, 45), (120, 45), (120, 25), (100, 35), (0, 0)]
-
 rThinbrowRight = polygon [(120, 25), (120, 45), (200, 30), (250, 20), (275, 10), (290, 5), (300, 0), (200, 10), (120, 25)]
 
-lThinbrow = pictures [lThinbrowLeft, lThinbrowRight]
-
+lThinbrow = translate (-50) 0 (pictures [lThinbrowLeft, lThinbrowRight])
 lThinbrowRight = polygon [(0, 0), (0, 20), (-100, 45), (-120, 45), (-120, 25), (-100, 35), (0, 0)]
-
 lThinbrowLeft = polygon [(-120, 25), (-120, 45), (-200, 30), (-250, 20), (-275, 10), (-290, 5), (-300, 0), (-200, 10), (-120, 25)]
 
 straightbrow = pictures [lStraightbrow, rStraightbrow]
 
-lStraightbrow = polygon [(0, 0), (-10, 40), (-200, 40), (-190, 0)]
+lStraightbrow = Translate (-75) 0 (polygon [(0, 0), (-10, 40), (-200, 40), (-190, 0)])
 
-rStraightbrow = polygon [(0, 0), (10, 40), (200, 40), (190, 0)]
+rStraightbrow = Translate 75 0 (polygon [(0, 0), (10, 40), (200, 40), (190, 0)])
