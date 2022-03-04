519cb56639000f1d79e52ca3bcc1f70f681ff291
dali0342 <david.lide.0342@student.uu.se>
2022-03-04 16:04:50 +0100

    More Specification and cleaning

--- Eyebrows.hs
+++ Eyebrows.hs
@@ -7,20 +7,24 @@ Function takes a string and returns a pictures of an eyebrow of their choice or 
 PRE: --
 SIDEEFFECTS: --
 RETURNS: picture of eyebrows or a blank picture
+EXAMPLES: generateEyeBrows 20 5 "3" green == pictures [color green (translate (-20) 5 lStraightBrow),color green (translate 20 5 rStraightbrow)]
+          generateEyeBrows 5  1 "p" blue  == Blank
 -}
 generateEyebrows :: Float -> Float -> String -> Color -> Picture
 generateEyebrows x y choice clr
   | choice == "1" = pictures [color clr (translate (- x) y lThinbrow), color clr (translate x y rThinbrow)]
   | choice == "2" = pictures [color clr (translate (- x) y lNormalbrow), color clr (translate x y rNormalbrow)]
   | choice == "3" = pictures [color clr (translate (- x) y lStraightbrow), color clr (translate x y rStraightbrow)]
-  | otherwise = Blank
+  | otherwise     = Blank
 
-eyebrows = pictures [normalbrow, thinbrow, straightbrow]
+eyebrows     = pictures [normalbrow, thinbrow, straightbrow]
+thinbrow     = pictures [lThinbrow, rThinbrow]
+normalbrow   = pictures [lNormalbrow, rNormalbrow]
+straightbrow = pictures [lStraightbrow, rStraightbrow]
 ------------------------------------------------------------------
 -- EVERYTHING BELOW IS JUST COORDS FOR ALL DOTS IN DIFFERENT EYEBROWS
 
 --EYEBROW 1
-thinbrow = pictures [lThinbrow, rThinbrow]
 
 rThinbrow = translate 50 0 (pictures [rThinbrowLeft, rThinbrowRight])
 rThinbrowLeft = polygon [(0, 0), (0, 20), (100, 45), (120, 45), (120, 25), (100, 35), (0, 0)]
@@ -32,7 +36,6 @@ lThinbrowLeft = polygon [(-120, 25), (-120, 45), (-200, 30), (-250, 20), (-275, 
 
 
 -- EYEBROW 2
-normalbrow = pictures [lNormalbrow, rNormalbrow]
 
 rNormalbrow = translate 50 0 (pictures [rNormalbrowLeft, rNormalbrowRight])
 rNormalbrowLeft = polygon [(0, 0), (0, 45), (100, 70), (120, 70), (120, 20), (100, 20), (60, 10), (0, 0)]
@@ -43,7 +46,6 @@ lNormalbrowRight = polygon [(0, 0), (0, 45), (-100, 70), (-120, 70), (-120, 20),
 lNormalbrowLeft = polygon [(-120, 70), (-200, 55), (-250, 35), (-275, 20), (-300, 0), (-200, 20), (-120, 20)]
 
 -- EYEBROW 3
-straightbrow = pictures [lStraightbrow, rStraightbrow]
 
 lStraightbrow = Translate (-75) 0 (polygon [(0, 0), (-10, 40), (-200, 40), (-190, 0)])
 rStraightbrow = Translate 75 0 (polygon [(0, 0), (10, 40), (200, 40), (190, 0)])
--- Hair.hs
+++ Hair.hs
@@ -2,8 +2,13 @@ module Hair where
 
 import Graphics.Gloss
 
-{-generateHair
-
+{-generateHair x y choiceOfHair clr
+    Creates a hair on the back of the head (Should be used with GenerateFringe)
+    PRE:--
+    SIDEFFECTS:--
+    RETURNS: A picture of the chosen hair in position x and y with the color clr or a blank picture
+    EXAMPLES: generateHair 1 2 "2" blue  == pictures [color blue (translate 1 2 longhair)]
+              generateHair 2 1 "u" green == Blank
 -}
 
 generateHair :: Float -> Float -> String -> Color -> Picture
@@ -16,8 +21,14 @@ generateHair xo yo choice clr
     x = 0 + xo
     y = 200 + yo
 
-{- generateFringe
-    The front part of generateHair
+{- generateFringe x-pos y-pos choiceOfHair clr
+    Creates the fringe of the hairdo (Should be used with generateHair)
+    PRE:--
+    SIDEEFFECTS:--
+    RETURNS: A picture of the chosen hair in position x and y with the color clr or a blank picture
+    EXAMPLES: generateFringe 2 1 "2" green == pictures [color green (translate 2 1 longFringe)]
+              generateFringe 9 4 "h" red   == Blank
+              generateFringe 1 2 "3" blue  == pictures [color baldColor (translate (1-200) 2 bald)]
 -}
 generateFringe :: Float -> Float -> String -> Color -> Picture
 generateFringe xo yo choice clr
@@ -29,13 +40,15 @@ generateFringe xo yo choice clr
     x = 0 + xo
     y = 200 + yo
 
-baldColor = withAlpha 0.5 white
 hair = pictures [spikyHair, longHair, bald]
+---------------------------------------------
 
+--HAIR 3
 bald = translate 0 75 (rotate 145 (scale 2 1 (circleSolid 50)))
+baldColor = withAlpha 0.5 white
 
+--HAIR 1
 spikyHair = pictures [translate 20 20 spikes, translate 0 0 spikes, translate (-10) (-10) spikes]
-
 spikes =
   pictures
     [ spike1,
@@ -48,14 +61,12 @@ spikes =
       translate 160 (-15) spike1,
       translate (-160) (-15) spike1
     ]
-
 spike1 = polygon [(-40, 0), (-20, 200), (0, 0)]
 
+--HAIR 2
 longHair = pictures [sideLeftHair, frontHair, sideRightHair]
 longFringe = arcSolid 0 180 400
 
 frontHair = translate 0 0 (scale 3 2 (circleSolid 200))
-
 sideLeftHair = translate (-500) 0 (scale 1.5 2 (circleSolid 200))
-
 sideRightHair = translate 500 0 (scale 1.5 2 (circleSolid 200))
