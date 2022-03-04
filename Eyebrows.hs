0cbfc19e2342cda473562fd2a6f9bb3902971153
dali0342 <david.lide.0342@student.uu.se>
2022-03-04 15:10:26 +0100

    Func specification and cleaning eyebrows

--- Eyebrows.hs
+++ Eyebrows.hs
@@ -2,8 +2,11 @@ module Eyebrows where
 
 import Graphics.Gloss
 
-{-generateEyebrows
-
+{-generateEyebrows x-pos y-pos choiceOfEyebrows color
+Function takes a string and returns a pictures of an eyebrow of their choice or a blank picture
+PRE: --
+SIDEEFFECTS: --
+RETURNS: picture of eyebrows or a blank picture
 -}
 generateEyebrows :: Float -> Float -> String -> Color -> Picture
 generateEyebrows x y choice clr
@@ -13,17 +16,10 @@ generateEyebrows x y choice clr
   | otherwise = Blank
 
 eyebrows = pictures [normalbrow, thinbrow, straightbrow]
+------------------------------------------------------------------
+-- EVERYTHING BELOW IS JUST COORDS FOR ALL DOTS IN DIFFERENT EYEBROWS
 
-normalbrow = pictures [lNormalbrow, rNormalbrow]
-
-rNormalbrow = translate 50 0 (pictures [rNormalbrowLeft, rNormalbrowRight])
-rNormalbrowLeft = polygon [(0, 0), (0, 45), (100, 70), (120, 70), (120, 20), (100, 20), (60, 10), (0, 0)]
-rNormalbrowRight = polygon [(120, 70), (200, 55), (250, 35), (275, 20), (300, 0), (200, 20), (120, 20)]
-
-lNormalbrow = translate (-50) 0 (pictures [lNormalbrowLeft, lNormalbrowRight])
-lNormalbrowRight = polygon [(0, 0), (0, 45), (-100, 70), (-120, 70), (-120, 20), (-100, 20), (-60, 10), (0, 0)]
-lNormalbrowLeft = polygon [(-120, 70), (-200, 55), (-250, 35), (-275, 20), (-300, 0), (-200, 20), (-120, 20)]
-
+--EYEBROW 1
 thinbrow = pictures [lThinbrow, rThinbrow]
 
 rThinbrow = translate 50 0 (pictures [rThinbrowLeft, rThinbrowRight])
@@ -34,8 +30,20 @@ lThinbrow = translate (-50) 0 (pictures [lThinbrowLeft, lThinbrowRight])
 lThinbrowRight = polygon [(0, 0), (0, 20), (-100, 45), (-120, 45), (-120, 25), (-100, 35), (0, 0)]
 lThinbrowLeft = polygon [(-120, 25), (-120, 45), (-200, 30), (-250, 20), (-275, 10), (-290, 5), (-300, 0), (-200, 10), (-120, 25)]
 
+
+-- EYEBROW 2
+normalbrow = pictures [lNormalbrow, rNormalbrow]
+
+rNormalbrow = translate 50 0 (pictures [rNormalbrowLeft, rNormalbrowRight])
+rNormalbrowLeft = polygon [(0, 0), (0, 45), (100, 70), (120, 70), (120, 20), (100, 20), (60, 10), (0, 0)]
+rNormalbrowRight = polygon [(120, 70), (200, 55), (250, 35), (275, 20), (300, 0), (200, 20), (120, 20)]
+
+lNormalbrow = translate (-50) 0 (pictures [lNormalbrowLeft, lNormalbrowRight])
+lNormalbrowRight = polygon [(0, 0), (0, 45), (-100, 70), (-120, 70), (-120, 20), (-100, 20), (-60, 10), (0, 0)]
+lNormalbrowLeft = polygon [(-120, 70), (-200, 55), (-250, 35), (-275, 20), (-300, 0), (-200, 20), (-120, 20)]
+
+-- EYEBROW 3
 straightbrow = pictures [lStraightbrow, rStraightbrow]
 
 lStraightbrow = Translate (-75) 0 (polygon [(0, 0), (-10, 40), (-200, 40), (-190, 0)])
-
 rStraightbrow = Translate 75 0 (polygon [(0, 0), (10, 40), (200, 40), (190, 0)])
